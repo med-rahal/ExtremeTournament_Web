7147896130988155a713d4cb028526c10cdc8763
[med-rahal] <[mohamed.rahal@esprit.tn]>
2022-05-13 04:09:30 +0100

    Integration final Runtime Terror

--- extreme_web/ExtremeTournament/ExtremeTournament/src/Form/AddProduitType.php
+++ extreme_web/ExtremeTournament/ExtremeTournament/src/Form/AddProduitType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,7 +20,13 @@ class AddProduitType extends AbstractType
             ->add('totalEnStock')
             ->add('descriptif')
             ->add('categorieProd')
-            ->add('disponibilite')
+            ->add('disponibilite',ChoiceType::class,array(
+                'choices'  => array(
+                    'disponible'   => 'disponible',
+                    'non disponible ' => 'non disponible',
+
+                )
+            ))
             ->add('image',FileType::class);
 
     }
